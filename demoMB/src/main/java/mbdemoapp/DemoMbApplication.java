b66dc64f8204a8ebd51e70f3e8e167ffc3179cb8
Khouja Boubaker <khouja.boubaker@yahoo.com>
2018-12-27 16:35:25 +0100

    comm comm

--- demoMB/src/main/java/mbdemoapp/DemoMbApplication.java
+++ demoMB/src/main/java/mbdemoapp/DemoMbApplication.java
@@ -4,6 +4,16 @@ package mbdemoapp;
 
 
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 
 
@@ -12,23 +22,35 @@ package mbdemoapp;
 
 
 
-
-
-
-
-public class DemoMbApplication  {
-	
-	
-	
-	
-	
-    
-
-	
-
-
-	
-	
+@SpringBootApplication
+public class DemoMbApplication implements CommandLineRunner {
 	
 
+
+	
+	
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMbApplication.class, args);
+	}
+	
+	
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+}
 }
